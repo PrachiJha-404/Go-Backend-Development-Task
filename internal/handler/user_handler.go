52165e808109b76340c3c4d576e29dad907af94f
PrachiJha-404 <prachijha1805@gmail.com>
2025-12-16 22:41:56 +0530

    feat(handler) add handlers for all 5 apis

---
+++ internal/handler/user_handler.go
@@ -0,0 +1,96 @@
+package handler
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+	"user-api/internal/models"
+	"user-api/internal/service"
+
+	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
+)
+
+type UserHandler struct {
+	service service.UserService
+	logger  *zap.Logger
+}
+
+func NewUserHandler(service service.UserService, logger *zap.Logger) *UserHandler {
+	return &UserHandler{
+		service: service,
+		logger:  logger,
+	}
+}
+
+func (h *UserHandler) ListUsers(c *fiber.Ctx) error {
+	dbUsers, err := h.service.ListUsers(c.Context())
+	if err != nil {
+		h.logger.Error("failed to list users", zap.Error(err))
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": "failed to fetch users"})
+	}
+	return c.Status(http.StatusOK).JSON(dbUsers)
+}
+
+func (h *UserHandler) GetUser(c *fiber.Ctx) error {
+	id, err := strconv.ParseInt(c.Params("id"), 10, 32)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "invalid user id"})
+	}
+	dbUser, err := h.service.GetUser(c.Context(), int32(id))
+	if err != nil {
+		h.logger.Error("failed to get user", zap.Error(err))
+		return c.Status(http.StatusNotFound).JSON(fiber.Map{"error": "user not found"})
+	}
+	return c.Status(http.StatusOK).JSON(dbUser)
+}
+
+func (h *UserHandler) CreateUser(c *fiber.Ctx) error {
+	var req models.CreateUserRequest
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "invalid request body"})
+	}
+	dob, err := time.Parse("2006-01-02", req.DOB)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "invalid date format (Use YYYY-MM-DD)"})
+	}
+	dbUser, err := h.service.CreateUser(c.Context(), req.Name, dob)
+	if err != nil {
+		h.logger.Error("failed to create user", zap.Error(err))
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": "failed to create user"})
+	}
+	return c.Status(http.StatusOK).JSON(dbUser)
+}
+
+func (h *UserHandler) UpdateUser(c *fiber.Ctx) error {
+	id, err := strconv.ParseInt(c.Params("id"), 10, 32)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "invalid request body"})
+	}
+	var req models.UpdateUserRequest
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "invalid request body"})
+	}
+	dob, err := time.Parse("2006-01-02", req.DOB)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "invalid data format (use YYYY-MM-DD)"})
+	}
+	user, err := h.service.UpdateUser(c.Context(), int32(id), req.Name, dob)
+	if err != nil {
+		h.logger.Error("failed to update user", zap.Error(err))
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": "failed to update user"})
+	}
+	return c.Status(http.StatusOK).JSON(user)
+}
+
+func (h *UserHandler) DeleteUser(c *fiber.Ctx) error {
+	id, err := strconv.ParseInt(c.Params("id"), 10, 32)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "invalid user id"})
+	}
+	err = h.service.DeleteUser(c.Context(), int32(id))
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": "failed to delete user"})
+	}
+	return c.Status(http.StatusOK).SendStatus(http.StatusNoContent)
+}
--- internal/models/user.go
+++ internal/models/user.go
@@ -8,3 +8,15 @@ type UserResponse struct {
 	DOB  time.Time `json:"dob"`
 	Age  int       `json:"age"`
 }
+
+// CreateUserRequest is what we expect from the user when they POST
+type CreateUserRequest struct {
+	Name string `json:"name"`
+	DOB  string `json:"dob"` // We keep this as string to parse it later
+}
+
+// UpdateUserRequest is what we expect when they PUT
+type UpdateUserRequest struct {
+	Name string `json:"name"`
+	DOB  string `json:"dob"`
+}
